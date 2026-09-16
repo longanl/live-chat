7f83275f94cbe0754f0c1aae3c3f255a5b39d444
fedora <xuziran687@gmail.com>
2026-09-16 18:50:50 +0800

    feat: 自定义 Swagger 接口地址（/docs + /api-docs）并放行拦截器

---
+++ backend/src/main/java/com/xuziran/livechat/common/config/WebMvcConfiguration.java
@@ -0,0 +1,74 @@
+package com.xuziran.livechat.common.config;
+
+import com.xuziran.livechat.common.interceptor.JwtTokenInterceptor;
+import com.xuziran.livechat.common.json.JacksonObjectMapper;
+import com.xuziran.livechat.common.properties.FileProperties;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Slf4j
+@Configuration
+@AllArgsConstructor
+// 继承WebMvcConfigurer接口启动拦截器
+public class WebMvcConfiguration implements WebMvcConfigurer{
+
+    private final JwtTokenInterceptor jwtTokenInterceptor;
+    private final FileProperties fileProperties;
+
+    // 注册拦截器
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("开始注册自定义拦截器...");
+        registry.addInterceptor(jwtTokenInterceptor)
+                .addPathPatterns("/**") // 拦截所有请求
+                .excludePathPatterns(
+                        "/user/login",
+                        "/user/register",
+                        "/uploadavatar687",
+                        "/files/upload",
+                        // Swagger 文档放行
+                        "/api-docs/**",
+                        "/docs/**"
+                );
+    }
+
+
+    // Swagger配置
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("在线聊天室")
+                        .version("1.0")
+                        .description("在线聊天室接口文档"));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + fileProperties.getPath());
+    }
+
+    //扩展消息转换器
+
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器...");
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将Java对象转为json
+        converter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+        converters.add(0,converter );
+    }
+}
